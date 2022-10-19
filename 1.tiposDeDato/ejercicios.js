cb83b982b267d190eaec9ad2a601ff74847186ef
niaveli <rivera.flores.2904@gmail.com>
2022-10-18 21:42:21 -0500

    Averigué y respondí a los ejercicios

--- 1.tiposDeDato/ejercicios.js
+++ 1.tiposDeDato/ejercicios.js
@@ -1,3 +1,7 @@
-//averiguar el methodo de javascript
+//averiguar el metodo de javascript
 //que me permite saber que tipo de dato
-//tengo
+//tengo:
+ // El método que me permite saber que tipo de dato tengo es el typeof, 
+ //este operador es muy útil porque nos permite consultar fácilmente el tipo de datos que una variable contiene.
+ // Esto es importante en JavaScript porque se trata de un lenguaje escrito dinámicamente.
+//
--- 2.variables/ejercicios.js
+++ 2.variables/ejercicios.js
@@ -1,5 +1,15 @@
-//averiguar las diferencias entre let y var
-//la cocina del código
-
+//averiguar las diferencias entre let y var:
+ //Let nos permite declarar variables limitando su alcance (scope) al bloque, 
+ //declaración, o expresión donde se está usando, mientras la palabra clave 
+ //Var, define una variable global o local en una función sin importar el ámbito del bloque.
+ //la cocina del código.
+//
+//Para entender:
+ //var: declara una variable de scope global o local para la función sin
+ //importar el ámbito de bloque. Permite hoisting.
+ 
+ //let: declara una variable de scope global, 
+ //local para la función o de bloque. Es reasignable y no permite hoisting.
+//
 //hosting, scope
 //contexto global y local
