e15fa40f7fb13a7a8ab1071ae11a28d42abf351b
niaveli <rivera.flores.2904@gmail.com>
2022-10-20 10:03:03 -0500

    Trabajo en equipo de los tipos de operadores de Javascript

---
+++ 3.operadores/ejercicios.js
@@ -0,0 +1,30 @@
+//deysidias56@gmail.com
+//ejemplos
+//Operadores Aritméticos:
+let a = 20;
+let b = 30;
+let suma=a+b
+console.log(suma)
+//
+let numero1 = 10;
+let numero2= 4;
+let result = numero1-numero2;
+console.log(result)
+//
+let c = 10;
+let d = 4;
+let multiplicación = c*d;
+console.log(multiplicación)
+//
+let e= 10;
+let f= 2;
+let división = e/f;
+console.log(división)
+//Operadores de Asignación:
+var contador = 1
+contador += 2
+contador -= 1
+contador *= 5
+contador /= 2
+console.log(contador)
+//
---
+++ 3.operadores/index.js
@@ -0,0 +1,13 @@
+//deysidias56@gmail.com
+//CONCEPTO Y LOS TIPOS DE OPERADORES:
+//OPERADORES: Los operadores permiten manipular el valor de las variables, 
+//realizar operaciones matemáticas con sus valores y comparar diferentes variables.
+// De esta forma, los operadores permiten a los programas realizar cálculos complejos
+// y tomar decisiones lógicas en función de comparaciones y otros tipos de condiciones.
+//TIPOS DE OPERADORES:
+//Operadores aritméticos: Se utilizan para efectuar operaciones matemáticas.
+ //Para realizar las operaciones básicas, como suma, resta, multiplicación y división.
+//Operador de Asignación: Un operador de asignación asigna un valor a su operando
+ // izquierdo basándose en el valor de su operando derecho, es decir el operador de asignación (=) 
+ //consiste en asignar un valor a una variable.
+//
