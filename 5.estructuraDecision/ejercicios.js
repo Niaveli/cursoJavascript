383c1dcd47d99136663898482e72edab279fac3f
niaveli <rivera.flores.2904@gmail.com>
2022-10-25 10:10:58 -0500

    Resolviendo los ejercicios

--- 5.estructuraDecision/ejercicios.js
+++ 5.estructuraDecision/ejercicios.js
@@ -4,3 +4,32 @@ if(vestido=="rosado"){
 }else{
     console.log("Me pongo mi vestido negro y no voy a ir a la fiesta ")
 }
+
+//EJERCICIO 1
+//Hacer un programa que apartir de una variable edad determine si es mayor o menor.
+var edad = 20
+if (edad<=23){
+console.log("Es mayor de edad")
+}else{
+    console.log("Es menor de edad")
+}
+//EJERCICIO 2
+//Hacer un programa que almacene en una variablenun paswsword y evalue si el password y evalue si el el 
+//password es correcto, imprima un mensaje de bienvenida, si es igual, y en mansaje de error si no lo es.
+contraseña="niavelirivera"
+password="niavelirivera"
+if(contraseña==password){
+    console.log("bienvenida")
+}else{
+    console.log("error")
+}
+//EJERCICIO 3
+//Hacer un programa que almacene los dividendo y divisor numeros si su divisor es cero se deberá mostrar
+//un mensaje de error caso contrario imprimirá el resultado de la división.
+let dividendo = 40
+let divisor = 10
+if (divisor ==0){
+    console.log("error")
+}else{
+    console.log(dividendo/divisor)
+}
