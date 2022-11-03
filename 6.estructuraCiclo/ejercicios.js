8b5ca8fee63bc8e64310a653e5802f4d17780a6c
niaveli <rivera.flores.2904@gmail.com>
2022-11-02 21:17:12 -0500

    Realicé las definiciones de for y while

---
+++ 6.estructuraCiclo/ejercicios.js
@@ -0,0 +1,24 @@
+//El ciclo for es uno de los más utilizados en programación debido a que permite repetir 
+//varias instrucciones (pasos) un cierto número de ocasiones (por ejemplo, 10 veces). 
+//Se emplea en el recorrido de vectores, matrices y estructuras, entre otros.
+//Sobre sus características se puede mencionar que:
+//Siempre se hace uso de una variable (contador) que incrementará su valor automáticamente y
+//ayudará a determinar si se continúa o finaliza el ciclo.
+//El contador deberá inicializarse con un valor, generalmente 0 ó 1, dependiendo de lo que se esté realizando.
+//Un ciclo puede contener otro ciclo dentro de sí (a esto se le denomina ciclo anidado). 
+//Nunca se debe utilizar el mismo nombre de la variable (contador) en ambos ciclos, pues 
+//el programa no podrá determinar cuándo se finaliza el ciclo.
+//ejemplo:
+for(var i = 0;i<5;i++){
+    console.log(i)
+}
+
+//El ciclo while La sentencia (hacer mientras) crea un bucle 
+//que ejecuta una sentencia especificada, hasta que la condición de comprobación 
+//se evalúa como falsa. La condición se evalúa después de ejecutar la sentencia,
+//dando como resultado que la sentencia especificada se ejecute al menos una vez.
+
+
+
+
+
