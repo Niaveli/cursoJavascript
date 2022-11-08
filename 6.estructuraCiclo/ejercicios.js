5f55adab4130a7f1213d5b8c6b3d586fb4e5d872
niaveli <rivera.flores.2904@gmail.com>
2022-11-07 22:44:47 -0500

    Resolviendo los ejercicios en estructura de ciclos y respondiendo la definición de funciones

--- 6.estructuraCiclo/ejercicios.js
+++ 6.estructuraCiclo/ejercicios.js
@@ -44,14 +44,35 @@ contador+=1
 }
 console.log('tienes', contador, 'vocales')
 
-// Un programa que me muestre el factorial de un número
-5!=4*3*2*1
-5!=120
 
-7!=7
-//hacer un programa que me determine si un texto es un palimdromo
+//EJERCICIOS:
+// 1. Un programa que me muestre el factorial de un número
+let numer='5'
+let total=1
+for(let i=1; i<=numer; i++){
+    total=total * i;
+    console.log(total);
+}
 
+// 2.hacer un programa que me determine si un texto es un palimdromo
+function palindromo (str){
+    for(let i =0 ; i<str.length; i++){
+        console.log(str[i] !== str[str.length-0-i])
+        if(str[i] !== str[str.length-1-i]){
+            return false
+        }
+    }
+    return true
+}
+palindromo("anita lava la tina")
 
-//hacer un programa que me muestre los n numeros de la serie 
+// 3.hacer un programa que me muestre los n numeros de la serie 
 //de fibonacci
-1-2-3-5-8-13
+let n1= 0;
+let n2= 1;
+for(let i=0; i<10; i++){
+    r= n1+n2;
+    n1=n2;
+    n2=r
+    console.log(r);
+}
---
+++ 7.funciones/index.js
@@ -0,0 +1,14 @@
+//Las funciones son la solución a todos estos problemas, tanto en 
+//JavaScript como en el resto de lenguajes de programación. Una función es 
+//un conjunto de instrucciones que se agrupan para realizar una tarea concreta
+// y que se pueden reutilizar fácilmente.
+//TIPOS DE FUNCIONES:
+ //1. Por declaración: Es probablemente la más utilizada y la más fácil de recordar, sobre todo si ya conoces algún 
+      //otro lenguaje de programación.Consiste en declarar la función con un nombre y sus parámetros 
+     //de entrada entre paréntesis.
+ //2. Por expresión: Este tipo ha tomado popularidad y consiste básicamente en guardar una función en una variable, para así ejecutar 
+     //la variable como si fuera una función. Este nuevo recurso ha dado pie a las funciones anónimas.
+ //3. Por constructor de objeto: La variante de constructor de objeto es la menos recomendada y casi no se utiliza.
+    //Únicamente nos ayuda a recordar que las funcionesMtambién son tipos de objetos en JavaScript.
+
+    
