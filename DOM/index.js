ce0c9b83bc8bcd724dff3a40d89da125420e0520
niaveli <rivera.flores.2904@gmail.com>
2022-11-17 11:07:58 -0500

    Definición de DOM

---
+++ DOM/index.js
@@ -0,0 +1,5 @@
+//El modelo de objeto de documento (DOM) es un interfaz para los documentos HTML y XML.
+// Facilita una representación destructurada del documento ty define de que manera los 
+//programas pueden acceder, al fin de modificar, tanto su estructura, estilo y contenido.
+//El DOM da una representación del documento como un grupo de nodos y objetos 
+//estructurados que tienen propiedades y métodos.
